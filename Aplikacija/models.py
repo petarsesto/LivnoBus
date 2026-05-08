58e961cf58989f4555480800f355c02c1d4bd4f5
petarsesto <petarsesto@ssklivno.net>
2026-05-08 09:56:33 +0200

    added homepage with lines display

--- Aplikacija/models.py
+++ Aplikacija/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Linija(models.Model):
+    naziv = models.CharField(max_length=100)
+    polaziste = models.CharField(max_length=100)
+    odrediste = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.polaziste} → {self.odrediste}"
+
+class VozniRed(models.Model):
+    linija = models.ForeignKey(Linija, on_delete=models.CASCADE)
+    vrijeme_polaska = models.TimeField()
+    vrijeme_dolaska = models.TimeField()
+
+    def __str__(self):
+        return f"{self.linija} ({self.vrijeme_polaska})"
--- Aplikacija/views.py
+++ Aplikacija/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Linija, VozniRed
 
-# Create your views here.
+def home(request):
+    linije = Linija.objects.all()
+    return render(request, 'home.html', {'linije': linije})
+
+from .models import VozniRed
+
+def vozni_red(request):
+    redovi = VozniRed.objects.all()
+    return render(request, 'vozniRed.html', {'redovi': redovi})
